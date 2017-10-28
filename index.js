c4ebea3e44a437e080fc9a87e2f023763694b7f9
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-28 20:40:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function theBeatlesPlay(musicians, instruments){
+  let array = [];
+
+  for (let i = 0; i<musicians.length; i++){
+    if (i===0){
+      array = [musicians[i] + " plays " + instruments[i]];
+    } else {
+      array = [...array, [musicians[i] + " plays " + instruments[i]]];
+    }
+    return array;
+  }
+}
