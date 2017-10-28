c177af0a1f5d9a890217507d3e051b3791f575ee
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-28 21:00:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,13 @@ function johnLennonFacts(facts){
   }
   return facts
 }
+
+function iLoveTheBeatles(number){
+  let array = [];
+
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while(number < 15);
+return array
+}
