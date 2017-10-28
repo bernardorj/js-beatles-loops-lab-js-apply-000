dfe59e74658adc48302edd1b2acd84898b5badec
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-28 20:46:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ function theBeatlesPlay(musicians, instruments){
   }
     return array
 }
+
+function johnLennonFacts(facts){
+  let i = 0;
+  
+  while (i<facts.length){
+    facts[i].push("!!!");
+  }
+}
